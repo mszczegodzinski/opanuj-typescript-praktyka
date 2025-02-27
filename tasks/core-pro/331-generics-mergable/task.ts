cf0375bc9ed64f06c6cbb182c4c4c395c0301808
Marek Szczegodziński <mszczegodzinski@o2.pl>
2025-02-27 23:38:00 +0100

    331-generics-mergable: implement MergableObject

--- tasks/core-pro/331-generics-mergable/task.ts
+++ tasks/core-pro/331-generics-mergable/task.ts
@@ -1,5 +1,10 @@
 // Zaimplementuj typ MergeableObject z wykorzystaniem typów wbudowanych - Exclude i NonNullable.
-type MergeableObject<T> = T;
+type Primitive = string | number | boolean | symbol | null | undefined;
+type NonObject = Primitive | Function | any[];
+
+type MergeableObject<T> = T extends NonObject
+  ? never
+  : { [K in keyof T]: NonNullable<Exclude<T[K], undefined>> };
 
 export function mergeObjects<T, U>(obj1: MergeableObject<T>, obj2: MergeableObject<U>): T & U {
   const merged = { ...obj1, ...obj2 };
